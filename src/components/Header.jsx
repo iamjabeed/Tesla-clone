46a3f542d2b9ba67a0facf596b9415c585e8bcfa
iamjabeed <smartyjabeedbhai001@gmail.com>
2023-04-25 22:56:13 +0530

    hemberg menu

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,10 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import styled from "styled-components";
 
 import { HiMenuAlt1, HiOutlineX } from "react-icons/hi";
-const Header = ({ isMenuOpen, setIsMenuOpen }) => {
+const Header = () => {
+  const [showBurger, setShowBurger] = useState(false);
   return (
     <Container>
       <Link to="/">
@@ -21,14 +22,26 @@ const Header = ({ isMenuOpen, setIsMenuOpen }) => {
       <RightMenu>
         <Link>Shop</Link>
         <Link>Account</Link>
-        <CustomeMenu>
-          {isMenuOpen ? (
-            <HiMenuAlt1 size={30} onClick={() => setIsMenuOpen(!isMenuOpen)} />
-          ) : (
-            <HiOutlineX size={30} onClick={() => setIsMenuOpen(!isMenuOpen)} />
-          )}
+        <CustomeMenu onClick={() => setShowBurger(!showBurger)}>
+          <HiMenuAlt1 size={24} />
         </CustomeMenu>
       </RightMenu>
+
+      <BurgerNav show={showBurger}>
+        <CloseWrap>
+          <CustomeClose onClick={() => setShowBurger(!showBurger)}>
+            <HiOutlineX size={24} />
+          </CustomeClose>
+        </CloseWrap>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+        <Link>Model s</Link>
+      </BurgerNav>
     </Container>
   );
 };
@@ -67,35 +80,66 @@ const Menu = styled.div`
   }
 
   @media (max-width: 820px) {
-    display:none;
+    display: none;
   }
 `;
 
 const RightMenu = styled.div`
   display: flex;
   align-items: center;
-  ${"" /* gap: 10px; */}
   a {
     transition: background 0.4s ease-in-out;
     text-transform: uppercase;
     font-weight: 500;
     font-size: 15px;
     padding: 6px 10px;
-    
-    ${'' /* @media (max-width: 520px) {
-    font-size: 13px;
-  } */}
-
   }
   a:hover {
     background: rgba(112, 128, 144, 0.4);
   }
 
-  @media (max-width: 820px) {
-
+  @media (max-width: 520px) {
   }
 `;
 
 const CustomeMenu = styled.div`
   cursor: pointer;
+`;
+
+const BurgerNav = styled.div`
+  position: fixed;
+  top: 0;
+  right: 0;
+  bottom: 0;
+  background: white;
+  display: flex;
+  flex-direction: column;
+  ${"" /* justify-content: space-between;  */}
+
+  width:240px;
+  padding: 0 20px;
+  gap: 20px;
+  z-index: 1;
+  transform: ${(props) => (props.show ? "translateX(100%)" : "translateX(0)")};
+  transition: transform 0.3s ease;
+  a {
+    border-bottom: 1px solid rgba(0, 0, 0, 0.5);
+    padding: 10px 0;
+    font-weight: 500;
+    font-size: 10px;
+    text-transform: uppercase;
+
+    @media (max-width: 820px) {
+      font-size: 12px;
+    }
+  }
+`;
+
+const CloseWrap = styled.div`
+  display: flex;
+  justify-content: flex-end;
+  cursor: pointer;
+`;
+const CustomeClose = styled.div`
+  margin-top: 20px;
 `;
